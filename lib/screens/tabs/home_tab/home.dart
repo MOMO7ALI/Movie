fe4dd0718c18a50f857491515ed21e16cbeb753e
MOMO7ALI <elm.10203040@gmail.com>
2024-09-09 22:07:17 +0300

    2 end commit_1

--- lib/screens/tabs/home_tab/home.dart
+++ lib/screens/tabs/home_tab/home.dart
@@ -5,6 +5,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SafeArea(child:Container() ),
+
+
+
+      ],
+    );
   }
 }
