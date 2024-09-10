567e0f975819b5142c678de156ffa42d0150ae5f
AbdoMansour <abdo.bedo2004@gmail.com>
2024-09-10 19:52:06 +0300

    brows tab

--- lib/screens/tabs/browse_tap/category_model.dart
+++ lib/screens/tabs/browse_tap/category_model.dart
@@ -1,15 +1,15 @@
-class CategoryModel{
-  String id;
-  String geName;
-  String image;
-
-  CategoryModel({required this.id, required this.geName, required this.image});
-
-
-  static List<CategoryModel> getCatorgery(id , name , imaage){
-
-
-  }
-
-
-}
+// class CategoryModel{
+//   String id;
+//   String geName;
+//   String image;
+//
+//   CategoryModel({required this.id, required this.geName, required this.image});
+//
+//
+//   static List<CategoryModel> getCatorgery(id , name , imaage){
+//
+//
+//   }
+//
+//
+// }
