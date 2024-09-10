a11db0c49789ed3e4dcad48af5220b1e1c4c45bd
MOMO7ALI <elm.10203040@gmail.com>
2024-09-10 08:32:07 +0300

    2 end commit_2

---
+++ lib/apis/api_manager.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:movies_app/apis/api_constants.dart';
+import 'package:movies_app/model/newrealeses_movies_response.dart';
+import 'package:movies_app/model/search_movies_response.dart';
+import 'package:movies_app/model/top_rated_movies_response.dart';
+import '../model/browse_genre_response.dart';
+import '../model/by_id_movies_response.dart';
+import '../model/movies_by_genre_response.dart';
+import '../model/popular_movies_response.dart';
+import '../model/similar_movies_response.dart';
+
+class ApiManager {
+  static Future<PopularMoviesResponse> getPopularMovies() async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.popularApi);
+    try {
+      var response = await http.get(url, headers: ApiConstants.popularHeaders);
+
+      if (response.statusCode == 200) {
+        return PopularMoviesResponse.fromJson(jsonDecode(response.body));
+      } else {
+        // Log the response status code for debugging
+        print("API Request Failed: ${response.statusCode}");
+        throw Exception("API Request Failed");
+      }
+    } catch (error) {
+      // Log the error for debugging
+      print("API Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<NewrealesesMoviesResponse> getNewRealeses() async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.newRealesesApi);
+    try {
+      var response =
+      await http.get(url, headers: ApiConstants.newRealsesHeaders);
+      if (response.statusCode == 200) {
+        return NewrealesesMoviesResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<SearchMoviesResponse> getSearchMovies(String query) async {
+    Uri url = Uri.https(
+        ApiConstants.baseUrl, ApiConstants.searchApi, {'query': query});
+    try {
+      var response = await http.get(url, headers: ApiConstants.searchHeaders);
+      if (response.statusCode == 200) {
+        return SearchMoviesResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<TopRatedMoviesResponse> getTopRatedMovies() async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.recommendedApi);
+    try {
+      var response =
+      await http.get(url, headers: ApiConstants.recommendedHeaders);
+      if (response.statusCode == 200) {
+        return TopRatedMoviesResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<BrowseGenreResponse> getBrowseGenre(String language) async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.browseGenreApi,
+        {'language': language});
+    try {
+      var response =
+      await http.get(url, headers: ApiConstants.browseGenreHeaders);
+      if (response.statusCode == 200) {
+        return BrowseGenreResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<MoviesByGenreResponse> getMoviesByGenre(String with_genres) async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.moviesByGenreApi,
+        {'with_genres': with_genres});
+    try {
+      var response =
+      await http.get(url, headers: ApiConstants.moviesByGenreHeaders);
+      if (response.statusCode == 200) {
+        return MoviesByGenreResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+
+
+  static Future<MoviesById> getMoviesById(String MovieID) async {
+    Uri url =
+    Uri.https(ApiConstants.baseUrl, ApiConstants.detailsApi + MovieID);
+    try {
+      var response = await http.get(url, headers: ApiConstants.detailsHeaders);
+      if (response.statusCode == 200) {
+        return MoviesById.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  static Future<SimilarMoviesResponse> getSimilarMovies(String MovieID) async {
+    Uri url = Uri.https(
+        ApiConstants.baseUrl, ApiConstants.similarApi + MovieID + '/similar');
+    try {
+      var response = await http.get(url, headers: ApiConstants.similarHeaders);
+      if (response.statusCode == 200) {
+        return SimilarMoviesResponse.fromJson(jsonDecode(response.body));
+      } else {
+        print('Api Request Failed: ${response.statusCode}');
+        throw Exception('Api Request Failed');
+      }
+    } catch (error) {
+      print("Api Request Error: $error");
+      throw error;
+    }
+  }
+
+  }
--- lib/screens/tabs/search_tap/search.dart
+++ lib/screens/tabs/search_tap/search.dart
@@ -1,10 +1,109 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import '../../../apis/api_manager.dart';
+import '../../../model/search_movies_response.dart';
+import '../../../themeing/app_theme.dart';
+import 'movie_search_item.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+  var searchResults = [];
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String query = '';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: AppTheme.black,
+      child: SafeArea(
+        child: Container(
+          height: double.infinity,
+          child: Column(
+            children: [
+              Container(), // Empty container can be removed
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  onChanged: (value) {
+                    query = value;
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppTheme.darkGrey,
+                    // Background color of the search bar
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: AppTheme.lightGrey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: AppTheme.lighterGrey,
+                    ), // Search icon
+                  ),
+                ),
+              ),
+              query.isEmpty
+                  ? Container(
+                      margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.27),
+                      child: Column(
+                        children: [
+                          Icon(Icons.local_movies,
+                              color: AppTheme.lighterGrey,
+                              size: MediaQuery.of(context).size.width * 0.35),
+                          Text(
+                            'No Movies Found',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          )
+                        ],
+                      ),
+                    )
+                  : FutureBuilder<SearchMoviesResponse>(
+                      future: ApiManager.getSearchMovies(query),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              color: AppTheme.gold,
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        } else {
+                          widget.searchResults = snapshot.data!.results ?? [];
+                          return Expanded(
+                            child: ListView.separated(
+                              itemBuilder: (context, index) {
+                                return MovieSearchItem(
+                                    movie: widget.searchResults[index]);
+                              },
+                              itemCount: widget.searchResults.length,
+                              separatorBuilder:
+                                  (BuildContext context, int index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Divider(
+                                    color: AppTheme.darkGrey,
+                                    thickness: 1,
+                                    height: 1,
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        }
+                      },
+                    )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
